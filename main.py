79bbd2f1276a0b59a934968fb2ebc2f2944eec4a
Arno Richter <arno.richter+github@gmail.com>
2022-11-05 22:29:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 def main():
-    my_input = os.environ["INPUT_HOST"]
+    my_input = os.environ["INPUT_SERVER"]
     
     user = os.environ["INPUT_USER"]
     key = os.environ["INPUT_PASS"]
@@ -12,6 +12,11 @@ def main():
     my_output = f"Hello {host} at port {port}"
 
     print(f"::set-output name=myOutput::{my_output}")
+    
+    
+    # subprocess.call(["rsync", "-Ccavz", "--delete","DJStatic", "username@website"])
+    with open("~/.ssh/id_rsa", "w") as text_file:
+        text_file.write("%s" % key)
 
 
 if __name__ == "__main__":
