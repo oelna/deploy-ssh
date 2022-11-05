2b0c2ef8b4b8af9e2cc490cd2f7eb7f09e74bc18
Arno Richter <arno.richter+github@gmail.com>
2022-11-05 22:42:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ def main():
     key = os.environ["INPUT_PASS"]
     host = os.environ["INPUT_SERVER"]
     port = os.environ["INPUT_PORT"]
+    
+    source = os.environ["INPUT_SOURCE"]
+    target = os.environ["INPUT_TARGET"]
 
     my_output = f"Hello {host} at port {port}"
 
     print(f"::set-output name=myOutput::{my_output}")
     
     
-    # subprocess.call(["rsync", "-Ccavz", "--delete","DJStatic", "username@website"])
+    
     
     keyfile = "~/.ssh/id_rsa"
     os.makedirs(os.path.dirname(keyfile), exist_ok=True)
     with open(keyfile, "w") as text_file:
         text_file.write("%s" % key)
-
+        
+    server_string = "%s@%s:%s" % (user, host, target)
+        
+    subprocess.call(["rsync", "-rltgoDzvO", "--dry-run", "./", server_string])
+    # rsync -Pav -e "ssh -i $HOME/.ssh/somekey" username@hostname:/from/dir/ /to/dir/
 
 if __name__ == "__main__":
     main()
