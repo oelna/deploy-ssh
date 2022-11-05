485f1a8a3a8496340ff5b81fba2d437d18e51b17
Arno Richter <arno.richter+github@gmail.com>
2022-11-05 22:21:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import os
 import requests
 
 def main():
-    my_input = os.environ["INPUT_USER"]
+    my_input = os.environ["INPUT_HOST"]
+    
+    user = os.environ["INPUT_USER"]
+    key = os.environ["INPUT_PASS"]
+    host = os.environ["INPUT_HOST"]
+    port = os.environ["INPUT_PORT"]
 
-    my_output = f"Hello {my_input}"
+    my_output = f"Hello {host} at port {port}"
 
     print(f"::set-output name=myOutput::{my_output}")
 
